e24e0449f0fa18b1a33bde91a090583285e77aa1
admin-asb <asb210297@gmail.com>
2023-05-29 23:00:15 +0600

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const API_KEY = 'b832db48-891d-49ca-a968-30ddf37e4219';
+export const MODAL_CLOSE_SEC = 2.5;
